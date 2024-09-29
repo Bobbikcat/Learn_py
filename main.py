a7a0cf467ec0bb9b91b7b82767deb58558fb98da
Kirill.dll <bobbikcat@gmail.com>
2024-09-29 18:16:17 -0400

    New commit

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
+# Elections
+user_input = ['y', 'Y', 'n', 'N']
+
+
+def check_input(arg_input):
+    user_pass = False
+    for i in user_input:
+        if i == arg_input:
+            user_pass = True
+            break
+    if not user_pass:
+        print('\nError: Invalid input. Try again..')
+        exit()
+
+
+def passing_score(_age, _country, _record):
+    age_issue = 'You are under 18'
+    country_issue = 'You are not a citizen'
+    record_issue = 'You have a criminal record'
+    issue_list = []
+
+    if _age == 'n' or _age == 'N':
+        issue_list.append(age_issue)
+    if _country == 'n' or _country == 'N':
+        issue_list.append(country_issue)
+    if _record == 'y' or _record == 'Y':
+        issue_list.append(record_issue)
+
+    if len(issue_list) <= 0:
+        print('\nVerification passed! You CAN vote!')
+    else:
+        print('\nVerification failed.. You CAN NOT vote.\nReasons:')
+        for reason in issue_list:
+            print(f'{reason}')
+
+
 print('(type: "Y" for YES, "N" for NO)\n')
 
 age = input('Are you 18 or older? -> ')
-if age == 'Y' or age == 'y':
-    country = input('Are you a citizen of Russia? -> ')
-    if country == 'Y' or country == 'y':
-        additional = input('Do you have a criminal record? -> ')
-        if additional == 'N' or additional == 'n':
-            print('\nПроверка пройдена: Вы можете проголосовать на выборах.')
-        elif additional == 'Y' or additional == 'y':
-            print('\nВам не разрешено голосовать.')
-        else:
-            print('\nОшибка ввода.. Начните заного.')
-    elif country == 'N' or country == 'n':
-        print('\nВам не разрешено голосовать.')
-    else:
-        print('\nОшибка ввода.. Начните заного.')
-elif age == 'N' or age == 'n':
-    print('\nВам не разрешено голосовать.')
-else:
-    print('\nОшибка ввода.. Начните заного.')
-    
+check_input(age)
+
+country = input('Are you a citizen of Russia? -> ')
+check_input(country)
+
+record = input('Do you have a criminal record? -> ')
+check_input(record)
+
+passing_score(age, country, record)
