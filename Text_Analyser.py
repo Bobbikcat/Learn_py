c48ef907445caa0d059b12881f3c173e9a1dc969
Kirill.dll <bobbikcat@gmail.com>
2024-10-02 08:03:25 -0400

    New commit

--- Text_Analyser.py
+++ Text_Analyser.py
@@ -1,85 +1,66 @@
 # Text Analyser
-
-vowels = list('aeiouyаеёиоуыэюя')
-vowels_count = 0
-new_string = ''
-long_word = ''
+cleared_string = ''
 words_list = []
 
 
-def input_handler():
-    global new_string
+def input_handler(str_input):
+    global cleared_string
     global words_list
-    global user_input
-    user_input = user_input.lower()
 
-    for el in user_input:
-        if el == el.upper() and el != ' ':
-            pass
-        else:
-            new_string += el
+    for el in str_input:
+        if el.isalpha() or el == ' ':
+            cleared_string += el
     else:
-        words_list = new_string.upper().split()
+        words_list = cleared_string.upper().split()
         print(f'Number of words: {len(words_list)}')
 
 
 def vowels_counter():
-    global vowels_count
-    for letter in new_string.lower():
-        for v in vowels:
-            if v == letter:
-                vowels_count += 1
-                break
-    print(f'Numbers of vowels: {vowels_count}')
+    vowels = 'aeiouyаеёиоуыэюя'
+    vowels_count = 0
+
+    for v in vowels:
+        vowels_count += cleared_string.count(v)
+    else:
+        print(f'Numbers of vowels: {vowels_count}')
 
 
 def longest_word():
-    global long_word
-    long_word = max(words_list, key=len)
-    print(f'The longest word: {long_word}')
+    if words_list:
+        long_word = max(words_list, key=len)
+        print(f'The longest word: {long_word}')
 
 
 def words_doubles():
-    repeated_words_amt = 0
-    repeated_words_list = [['none']]
-    temp_list = []
-    match = 0
+    if words_list:
+        repeated_words_amt = 0
+        unique_words = set(words_list)
 
-    for word in words_list:
-        for current in words_list:
-            if current == word:
-                repeated_words_amt += 1
-        else:
-            temp_list = [word, repeated_words_amt]
-            for value in repeated_words_list:
-                if value == temp_list:
-                    match += 1
-            else:
-                if match < 1:
-                    repeated_words_list.append(temp_list)
-                repeated_words_amt = 0
-                match = 0
-    else:
         print('\nWord Statistic:\n')
-        _bool = False
-        for element in repeated_words_list:
-            if _bool:
-                if element[1] > 1:
-                    print(f'" {element[0]} ": {element[1]} words')
-                else:
-                    print(f'" {element[0]} ": {element[1]} word')
+        for word in unique_words:
+            repeated_words_amt += words_list.count(word)
+            temp_list = [word, repeated_words_amt]
+            repeated_words_amt = 0
+
+            if temp_list[1] > 1:
+                print(f'" {temp_list[0]} ": {temp_list[1]} words')
             else:
-                _bool = True
-                continue
+                print(f'" {temp_list[0]} ": {temp_list[1]} word')
 
 
 # 'Run program'
-user_input = input('Type something for Analyse: ')
-hash_input = hash(user_input)
-print('\nANALYSING..')
-print(f'operation id: {hash_input}\n')
+while True:
+    user_input = input('Type something for Analyse: ')
+    if user_input:
+        hash_input = hash(user_input)
+        print('\nANALYSING..')
+        print(f'operation id: {hash_input}\n')
 
-input_handler()
-vowels_counter()
-longest_word()
-words_doubles()
+        input_handler(user_input)
+        vowels_counter()
+        longest_word()
+        words_doubles()
+        break
+    else:
+        print('Error: No input taken! Try again\n')
+        continue
