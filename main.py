e3a8b00b06a35399fc51e8ce6ffa7c01e3f08b98
Kirill.dll <bobbikcat@gmail.com>
2024-10-02 14:07:59 -0400

    New commit

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-# Sum of Even Numbers
-even_sum = 0
+# Sum of Even Numbers. (обернул, но зачем ?)
+from itertools import cycle
+from math import trunc
 
-for i in range(1, 100):
-    if i % 2 == 0:
-        even_sum += i
-print(even_sum)
+
+def even_sum():
+    _even = 0
+    for i in range(1, 100):
+        if i % 2 == 0:
+            _even += i
+    return _even
+
+
+result = even_sum()
+print(result)
 
 # Squares of odd numbers
 odd_list = [n ** 2 for n in range(1, 10) if n % 2 != 0]
@@ -13,25 +21,20 @@ print(odd_list)
 # Negative numbers counter
 nums_amt = 0
 
-
-def cycle_attempt():
-    global nums_amt
-    pass_bool = True
+while True:
     try:
+        _bool = True
         user_input = float(input('\nEnter a negative number: '))
     except ValueError:
-        print('Error: Invalid input.')
-        pass_bool = False
+        print('ERROR: Invalid input. Try again..\n')
+        _bool = False
     finally:
-        if pass_bool:
+        if _bool:
             if user_input == abs(user_input):
                 nums_amt += 1
-                print('ENTERED: ' + str(user_input))
+                print(f'ENTERED: {user_input}')
                 print('Not a negative number! Try again..')
-                cycle_attempt()
             else:
-                print('ENTERED: ' + str(user_input))
-                print('Attempts: ' + str(nums_amt + 1))
-
-
-cycle_attempt()
+                print(f'ENTERED: {user_input}')
+                print(f'Entered numbers: {nums_amt + 1}')
+                break
