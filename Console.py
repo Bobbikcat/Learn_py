32ada1034feeeaa702065cdfd7f84e199f483342
Kirill.dll <bobbikcat@gmail.com>
2024-10-08 15:09:33 -0400

    New commit

--- Console.py
+++ Console.py
@@ -9,17 +9,19 @@ class Console:
 
     def book_list_view(self):
         all_books = self.service.view_library()
-        books_list = []
-        book_index = 0
 
         if all_books:
             print('\nLIBRARY BOOKS:\n')
-            for book in all_books:
-                books_list.append(book)
-                print(f'[{book_index + 1}] {books_list[book_index]}')
-                book_index += 1
+            for index, (k, v) in enumerate(all_books.items(), start=1):
+                print(f'[{index}] {k}')
         else:
             print('\n- The library of books is empty -\nreturn to user menu..\n')
+
+    def update_book(self, title, book):
+        user_choice = input('Would you like to UPDATE this book ?\n1. YES\n2. NO (press any key)\n>>> ')
+        if user_choice.strip() == '1':
+            x = self.service.update_book(title, book)
+            print(f'\nYOUR BOOK "{x}" WAS UPDATED !\n')
 
     def add_book(self):
         new_title = input('\nEnter a new book - TITLE: ')
@@ -33,10 +35,7 @@ class Console:
             print(empty)
         except NameError as exist:
             print(exist)
-            user_choice = input('Would you like to UPDATE this book ?\n1. YES\n2. NO (press any key)\n>>> ')
-            if user_choice.strip() == '1':
-                x = self.service.update_book(new_book.title, new_book)
-                print(f'\nYOUR BOOK "{x}" WAS UPDATED !\n')
+            self.update_book(new_title, new_book)
 
     def remove_book(self):
         delete_book = input('\nEnter a NAME of the book: ').strip()
@@ -47,47 +46,13 @@ class Console:
             print(e)
 
     def find_book(self):
-        book = input('\nEnter a NAME of the book: ').strip()
+        book_name = input('\nEnter a NAME of the book: ').strip()
         try:
-            d = self.service.find_book(book)
-            print(f'\nThe book - " {book} "\nAuthor: {d[0]}\nReleased in {d[1]}')
-            if d[2]:
-                self.change_status(book, True)
-            elif d[2] is False:
-                self.change_status(book, False)
-            else:
-                self.change_status(book, None)
+            d = self.service.find_book(book_name)
+            print(f'\nThe book - " {book_name} "\nAuthor: {d[0]}\nReleased in {d[1]}')
+            self.service.change_status(book_name, d[2])
         except NameError as nothing:
             print(nothing)
-
-    def change_status(self, book, status):
-        if status:
-            print('\nIN-STOCK: YES')
-            avlb = input('Mark this book <Unavailable> ? enter " Y/N ": ')
-            if avlb.upper().strip() == 'Y':
-                self.service.change_status(book, False)
-                print('\n*Now your book NOT-in-Stock*')
-            else:
-                print('\n*return to menu*')
-        elif status is False:
-            print('\nIN-STOCK: NO')
-            avlb = input('Mark this book <Available> ? enter " Y/N ": ')
-            if avlb.upper().strip() == 'Y':
-                self.service.change_status(book, True)
-                print('\n*Now your book IN-Stock*')
-            else:
-                print('\n*return to menu*')
-        else:
-            avlb = input(
-                '''\n> Book in the Library, but status not defined <\nTo set your status, enter:\n"A" - Available | "U" - Unavailable | *press any key to leave*: ''')
-            if avlb.upper().strip() == 'A':
-                self.service.change_status(book, True)
-                print('\n*Now your book IN-Stock*')
-            elif avlb.upper().strip() == 'U':
-                self.service.change_status(book, False)
-                print('\n*Now your book NOT-in-Stock*')
-            else:
-                print('\n*return to menu*')
 
     # Menu for user input
     def run(self):
--- Service.py
+++ Service.py
@@ -26,8 +26,34 @@ class Service:
 
     def find_book(self, book):
         if book in self.repository.view_library():
-            return list(self.repository.view_library()[book].values())  # да, это гениально, спасибо :3
+            return list(self.repository.view_library()[book].values())
         raise NameError('\nCan\'t find this book now...\nreturn to user menu..\n')
 
-    def change_status(self, book, status):
-        self.repository.change_status(book, status)
+    def change_status(self, book_name, status):
+        if status:
+            print('\nIN-STOCK: YES')
+            available = input('Mark this book <Unavailable> ? enter " Y/N ": ')
+            if available.upper().strip() == 'Y':
+                self.repository.change_status(book_name, False)
+                print('\n*Now your book NOT-in-Stock*')
+            else:
+                print('\n*return to menu*')
+        elif status is False:
+            print('\nIN-STOCK: NO')
+            available = input('Mark this book <Available> ? enter " Y/N ": ')
+            if available.upper().strip() == 'Y':
+                self.repository.change_status(book_name, True)
+                print('\n*Now your book IN-Stock*')
+            else:
+                print('\n*return to menu*')
+        else:
+            available = input(
+                '''\n> Book in the Library, but status not defined <\nTo set your status, enter:\n"A" - Available | "U" - Unavailable | *press any key to leave*: ''')
+            if available.upper().strip() == 'A':
+                self.repository.change_status(book_name, True)
+                print('\n*Now your book IN-Stock*')
+            elif available.upper().strip() == 'U':
+                self.repository.change_status(book_name, False)
+                print('\n*Now your book NOT-in-Stock*')
+            else:
+                print('\n*return to menu*')
