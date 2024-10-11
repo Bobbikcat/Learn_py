84f2aadaff31024125a00ac8ca11c7bd28a3a49f
Kirill.dll <bobbikcat@gmail.com>
2024-10-11 17:36:58 -0400

    New commit

--- area.py
+++ area.py
@@ -1,61 +1,34 @@
-import math
+from math import pi
+from abc import abstractmethod
 
 
 class Shape:
-    __rect = 0
-    __circ = 0
+    def __init__(self, size=0):
+        self.size = size
 
+    @abstractmethod
     def area(self, *args):
-        try:
-            for el in args:
-                if type(el) != int and type(el) != float:
-                    raise Exception('Error: Values must be int or float..')
-
-            if 0 < len(args) < 2:
-                print('\n1 value received\nSet as "Diameter"')
-                self.__circ = args[0] ** 2 / math.pi * 4
-                return self.__circ
-            elif 0 < len(args) < 3:
-                print('\n2 values received\nSet as "axis:x, axis:y"')
-                self.__rect = math.prod(list(args))
-                return self.__rect
-            else:
-                print('\nError: Numbers of Values is Out of Range')
-                return None
-        except (TypeError, ValueError):
-            print('Error: Values must be int or float..')
+        return self.size
 
 
 class Rectangle(Shape):
-    # pass
-    def area(self, x=0, y=0):
-        if type(x) == int or type(x) == float:
-            if type(y) == int or type(y) == float:
-                print('\nRectangle:')
-                return x * y
-        raise Exception('Error: Values must be int or float..')
+    def area(self, w=0, h=0):
+        return w * h
 
 
 class Circle(Shape):
-    def area(self, value=0):
-        if type(value) == int or type(value) == float:
-            print('\nCircle:')
-            return value ** 2 / math.pi * 4  # S = D^2 / 4Pi
-        raise Exception('Error: Values must be int or float..')
+    def area(self, r=0):
+        return pi * r ** 2
 
 
 if __name__ == '__main__':
-    shape = Shape()
+    shape = Shape(size=666)
+    print(f'Size of Shape: {shape.area()}')
+
     s_rect = Rectangle()
+    _rectangle = s_rect.area(5, 5)
+    print(f'Rectangle Area: {_rectangle}')
+
     s_circ = Circle()
-
-    _rect = s_rect.area(3, 4)
-    print(f'The Area of Shape: {_rect}')
-    print('_' * 60)
-
-    _circ = s_circ.area(10)  # По Диаметру
-    print(f'The Area of Shape: {_circ}')
-    print('_' * 60)
-
-    s_main = shape.area(2, 5)  # Круг или Прямоугольник
-    print(f'\nThe Area of Shape: {s_main}')
+    _circle = s_circ.area(10)
+    print(f'Circle Area: {_circle}')
