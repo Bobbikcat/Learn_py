9e1d833799e3975626bcf2dbc4c319076fb8631a
Kirill.dll <bobbikcat@gmail.com>
2024-10-14 21:28:08 -0400

    New commit

--- hw_files/main.py
+++ hw_files/main.py
@@ -1,61 +1,43 @@
 def read_file(file):
-    if checker(file):
-        with open(file, 'r', encoding='utf-8') as reading:
-            data_read = reading.read()
-            print(f'\n{data_read}\n')
-
-
-def write_file(adding, to_file):
-    if checker(adding) and checker(to_file):
-        with open(adding, 'r', encoding='utf-8') as file_new:
-            lines = file_new.readlines()
-        with open(to_file, 'a', encoding='utf-8') as file_target:
-            file_target.write('\n\n')
-            for el in lines:
-                file_target.write(f'{el}')
+    with open(file, 'a+', encoding='utf-8') as f:
+        f.seek(0)
+        data = f.read()
+        print(f'\n{data}\n')
 
 
 def read_lines(file):
-    if checker(file):
-        with open(file, 'r', encoding='utf-8') as file_lines:
-            for line in file_lines:
-                print(line.strip())
+    with open(file, 'a+', encoding='utf-8') as lines:
+        lines.seek(0)
+        for line in lines:
+             print(line.strip())
 
 
-def copy_binary(to_copy):
-   if checker(to_copy):
-        with open(to_copy, 'rb') as copy:
-            binary_data = copy.read()
-        with open('data_binary.txt', 'wb') as binary_file:
-            binary_file.write(binary_data)
+def write_file(text_new, file):
+        with open(file, 'a', encoding='utf-8') as content:
+            content.write('\n\n')
+            content.write(text_new)
 
-def checker(text_file):
-    try:
-        test = open(text_file, 'r', encoding='utf-8')
-        test.close()
-        return True
-    except OSError: # Он ведь Parent для файловых исключений или нет ?
-        print(f'Error: File "{text_file}" error..')
-        exit('End program')
-        # Не понимаю, почему тут eсли переменная bool,
-        # то он выдает OSError системным красным цветом.
-        # Eсли переменной нет, выдает Name Error.
-        # Отлов этими разными эксэптами не помогает, и
-        # так же не помогает ни Exeption, ни BaseExeption...
-        # как ни крути - не ловит, и выдает красным цветом ошибки.
+
+def copy_binary(file_copy):
+        with open(file_copy, 'rb') as copy:
+            data = copy.read()
+        with open('data.bin', 'wb') as binary:
+            binary.write(data)
+
+
 
 if __name__ == '__main__':
-    main_file = 'data.txt'
-    new_file = 'data_1.txt'
+    text_file = 'data.txt'
+    read_file(text_file)
 
-    read_file(main_file)
-    input('\nNext step: ')
+    input('\nNext step - press [Enter]')
+    user_input = input('\nWrite something to add this\n>>>')
 
-    write_file(adding=new_file, to_file=main_file)
-    read_file(main_file)
-    input('\nNext step: ')
+    write_file(text_new=user_input, file=text_file)
+    read_file(text_file)
+    input('\nNext step - press [Enter]\n')
 
-    read_lines(main_file)
-    input('\nNext step: ')
+    read_lines(text_file)
+    input('\nNext step - press [Enter]\n')
 
-    copy_binary(main_file)
+    copy_binary(text_file)
