270a97d8a2acdef03a54cc3dc37d866558a227a8
Kirill.dll <bobbikcat@gmail.com>
2024-10-20 12:23:39 -0400

    New commit

--- Task_Manager/task_manager.py
+++ Task_Manager/task_manager.py
@@ -1,4 +1,5 @@
 import json
+from user_menu import UserMenu
 
 
 class TaskManager:
@@ -7,10 +8,9 @@ class TaskManager:
 
     def view_tasks(self):
         if self.tasks:
-            for i, el in enumerate(self.tasks, start=1):
-                print(f'{i}. {el['description']} | Status: {el['completed']}')
-            else:
-                return True
+            for index, el in enumerate(self.tasks, start=1):
+                print(f'{index}. {el['description']} | Status: {el['completed']}')
+            return True # _____________№1 Имеется ввиду, убрать просто слово else?
         else:
             print('- Empty -')
             return None
@@ -38,14 +38,14 @@ class TaskManager:
 
     def save_to_json(self, filename):
         with open(f'{filename}.json', 'w') as file:
-            json.dump(self.tasks, file, indent=4)  # все еще не понимаю, почему ругается
+            json.dump(self.tasks, file, indent=4)  #_______ №2 все еще не понимаю
             print('* Saved *')
 
     def load_from_json(self, filename):
         try:
             with open(f'{filename}.json', 'r') as file:
                 self.tasks = json.load(file)
-        except (json.JSONDecodeError, FileNotFoundError) as e:
+        except (json.JSONDecodeError, FileNotFoundError):
             print('Error: File: Invalid / Not Found')
 
 
@@ -53,15 +53,9 @@ if __name__ == '__main__':
     tasker = TaskManager()
     while True:
         print('\nTASKER\n')
-        print('\n1. Tasks View')
-        print('2. Task Add')
-        print('3. Task Remove')
-        print('4. Task Complete')
-        print('5. Save to JSON')
-        print('6. Load from JSON')
-        print('Q. Exit')
-        user_input = input(f'\nEnter: ')
-        print('____________')
+        for i, value in UserMenu.menu.items():
+            print(f'{i} {value}')
+        user_input = input(f'\n__________\nEnter: ')
 
         if user_input.strip() == '1':
             tasker.view_tasks()
---
+++ Task_Manager/user_menu.py
@@ -0,0 +1,10 @@
+class UserMenu:
+    menu = {
+        '1.': 'Tasks View',
+        '2.': 'Task Add',
+        '3.': 'Task Remove',
+        '4.': 'Task Complete',
+        '5.': 'Save to JSON',
+        '6.': 'Load from JSON',
+        'Q.': 'Exit'
+    }
